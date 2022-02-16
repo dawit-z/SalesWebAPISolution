799e326edd2a64ce766a66ca0c2522eb252111cd
Dawit <Dzewelday@me.com>
2022-02-16 16:19:02 -0500

    Add project files.

---
+++ SalesWebAPI/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        [Required, MaxLength(2)]
+        public string StateCode { get; set; }
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Sales { get; set; }
+
+        public Customer()
+        {
+               
+        }
+    }
+}
